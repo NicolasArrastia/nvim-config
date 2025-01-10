ea9a774467abab4ae1a62329518053766830c7f6
Nicolas <nicolasarrastia@gmail.com>
2025-01-10 11:05:41 -0300

    Add options and comments

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,4 +1,6 @@
-return { -- Adds git related signs to the gutter, as well as utilities for managing changes
+return {
+	-- Adds git related signs to the gutter, as well as utilities for managing changes
+	-- Similar to gitlens
 	"lewis6991/gitsigns.nvim",
 	opts = {
 		signs = {
